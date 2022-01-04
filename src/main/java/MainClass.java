c9ead11bdac3afa7cd39e9ddb7baae489e0920cb
KurkinSerg <kurkin.serg@gmail.com>
2022-01-04 19:53:45 +0200

    HW_3 testGetClassString

--- src/main/java/MainClass.java
+++ src/main/java/MainClass.java
@@ -1,5 +1,10 @@
 public class MainClass {
     private final int class_number = 20;
+    private final String class_string = "Hello, world";
+
+    public String getClassString() {
+        return class_string;
+    }
 
     public int getClassNumber() {
         return this.class_number;
--- src/test/java/MainClassTest.java
+++ src/test/java/MainClassTest.java
@@ -6,11 +6,20 @@ public class MainClassTest {
 
     @Test
     public void testGetLocalNumber() {
-        Assertions.assertEquals(14, mainClass.getLocalNumber(), "Recieved value is not equal 14");
+        Assertions.assertEquals(14, mainClass.getLocalNumber()
+                , "Recieved value is not equal 14");
     }
 
     @Test
     public void testGetClassNumber() {
-        Assertions.assertTrue(mainClass.getClassNumber() > 45, "The value that has been recieved from function 'getClassNumber' is less than 45");
+        Assertions.assertTrue(mainClass.getClassNumber() > 45
+                , "The value that has been recieved from function 'getClassNumber' is less than 45");
+    }
+
+    @Test
+    public void testGetClassString() {
+        String searchText = mainClass.getClassString();
+        Assertions.assertTrue(searchText.contains("hello") || searchText.contains("Hello")
+                , "The value that has been recieved from function 'getClassString' doesn't contains words 'hello' or 'Hello'");
     }
 }
